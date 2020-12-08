dae3153b93e0ef14931789d142a8f9db30b05b15
Emre USTA <emre.usta@stu.fsm.edu.tr>
2020-12-09 01:40:40 +0300

    Updated

--- anim/WriteName_Emre.js
+++ anim/WriteName_Emre.js
@@ -3,7 +3,7 @@ class WriteName_Emre extends Animator {
         super();
         this.author = 'Emre USTA'
         this.desc = 'Writing the First Letters of My Name and Last Name'
-        let b = ["pink", "darkred", "blue", "yellow", "gray", "black", "cyan"]
+        let bg = ["pink", "darkred", "blue", "yellow", "gray", "black", "cyan"]
         let x0 = "translate(0,0)",
             x1 = "translate(500px,0px)",
             x2 = "translate(0px,0px)",
@@ -17,7 +17,7 @@ class WriteName_Emre extends Animator {
             x10 = "translate(0px,500px)",
             x11 = "translate(500px,500px)",
             x12 = "translate(500px,0px)"
-        this.anim = { background: b, transform: [x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12] }
+        this.anim = { background: bg, transform: [x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12] }
         this.elt = document.createElement('div')
         this.elt.style = "width: 50px; height: 50px; background-color: Black; border-radius : 40%;"
         this.time = 10000
